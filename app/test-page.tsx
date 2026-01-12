2592ff14e447e576651cf28ebdd8c4dcde1483f4
ollihietanen-blip <ollihietanen-blip@users.noreply.github.com>
2026-01-12 08:18:46 +0200

    Remove test page

--- app/test-page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: '20px', backgroundColor: '#fff', color: '#000' }}>
-      <h1>Testi - Jos näet tämän, renderöinti toimii</h1>
-      <p>Tämä on yksinkertainen testisivu ilman komponentteja.</p>
-    </div>
-  )
-}
